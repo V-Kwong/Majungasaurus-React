3e1b7c782beb0141055f2336c2f74224da617bd7
Vincent Kwong <vincent_kwong578@hotmail.com>
2020-07-19 12:20:27 -0700

    Updates

---
+++ precache-manifest.732878a9d4781ac49921412f6620ba04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1645f9c31aeda334233beb567637ab",
+    "url": "/Majungasaurus-React/index.html"
+  },
+  {
+    "revision": "08621c91d61f13ba2b8b",
+    "url": "/Majungasaurus-React/static/css/main.3194dfc3.chunk.css"
+  },
+  {
+    "revision": "a3753e5c110cac646052",
+    "url": "/Majungasaurus-React/static/js/2.cb45a152.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Majungasaurus-React/static/js/2.cb45a152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08621c91d61f13ba2b8b",
+    "url": "/Majungasaurus-React/static/js/main.a56a44a7.chunk.js"
+  },
+  {
+    "revision": "bb53e44c41e8e351d96f",
+    "url": "/Majungasaurus-React/static/js/runtime-main.39029b49.js"
+  },
+  {
+    "revision": "f9ff876f3261fdfb7602e9195b1104a3",
+    "url": "/Majungasaurus-React/static/media/Oxygen-Regular.f9ff876f.ttf"
+  },
+  {
+    "revision": "ff565b0f206bf63e82cb1003b7a80316",
+    "url": "/Majungasaurus-React/static/media/black-dinosaur-logo-clip-art-png-clip-art.ff565b0f.png"
+  }
+]);
