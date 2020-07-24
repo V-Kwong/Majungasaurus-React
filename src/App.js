99d65f285ed2dcd0897741a19b088a219bfa6d9b
Vincent Kwong <vincent_kwong578@hotmail.com>
2020-07-23 18:31:08 -0700

    added sticky footer component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react"
 import { Scrollbars } from 'react-custom-scrollbars';
 import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import InstagramIcon from '@material-ui/icons/Instagram';
-// import { useStaticQuery, graphql } from "gatsby"
-// import Img from "gatsby-image"
-// import { Link } from "gatsby"
 
 import styles from './App.module.css';
+
+import SticktyFooter from './components/stickyFooter';
+
 import Page from './components/page';
 import Main from './screens/main';
 import About from './screens/about';
@@ -33,12 +30,12 @@ export default class App extends Component {
     return (
       <Scrollbars
         ref='scrollbars'
-        style={{ width: '100vw', height: '100vh' }}
+        style={{ width: '100vw', height: '100vh', backgroundColor: 'black' }}
       >
 
-        <Page>
+        {/* <Page> */}
           <Main />
-        </Page>
+        {/* </Page> */}
       
         <LogoButton>
           <img src={Logo} className={styles.logo} alt="" onClick={this.scrollToTop}/>
@@ -57,68 +54,33 @@ export default class App extends Component {
           <li data-menuanchor="contact"><a href="#contact">Contact</a></li>
         </ul> */}
 
-        <Page>
-          <div className="pageDividerTop"/>
-          <About />
-          <div className="pageDividerBottom"/>
-        </Page>
+        {/* <Page> */}
+        <div className="pageDividerTop"/>
+        <About />
+        <SticktyFooter/>
+        {/* </Page> */}
 
-        <div className={styles.overlay}>
-          <div className={styles.social}>
-            <a href="#" target="_blank">
-              <span className={styles.socialIconCircle}>
-                <FacebookIcon
-                  className={styles.socialIcon}
-                  fontSize='small'
-                  color='inherit'
-                />
-              </span>
-            </a>
-            <a href="#" target="_blank">
-              <span className={styles.socialIconCircle}>
-                <TwitterIcon
-                  className={styles.socialIcon}
-                  fontSize='small'
-                  color='inherit'
-                />
-              </span>
-            </a>
-            <a href="#" target="_blank">
-              <span className={styles.socialIconCircle}>
-                <InstagramIcon
-                  className={styles.socialIcon}
-                  fontSize='small'
-                  color='inherit'
-                />
-              </span>
-            </a>
-          </div>
-          <div>
-            <p className={styles.copyright}> © 2020 Majungasaurus. All rights reserved.</p>
-          </div>
-        </div>
-
-        <Page>
+        {/* <Page> */}
           <div className="pageDividerTop"/>
           <Product
             title='Brachiosaurus'
             text='Most Iconic and initially thought to be one of the Largest Dinosaurs.'
           />
           <div className="pageDividerBottom"/>
-        </Page>
-        <Page>
+        {/* </Page>
+        <Page> */}
           <div className="pageDividerTop"/>
           <Product
             title='Edmontosaurus'
             text='Herbivore that could move on both Two Legs and Four. Flex.'
           />
           <div className="pageDividerBottom"/>
-        </Page>
-        <Page>
+        {/* </Page>
+        <Page> */}
           <div className="pageDividerTop"/>
           <Contact />
           <div className="pageDividerBottom"/>
-        </Page>
+        {/* </Page> */}
       </Scrollbars>
     )
   }
---
+++ src/components/stickyFooter.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import InstagramIcon from '@material-ui/icons/Instagram';
+
+import styles from './sticktyFooter.module.css';
+
+export default class SticktyFooter extends Component {
+  render() {
+    return (
+      <div className={["pageDividerBottom", styles.container].join(' ')}>
+        <div className={styles.social}>
+          <a href="https://www.youtube.com/watch?v=_65kfGhZXZw" target="_blank">
+            <span className={styles.socialIconCircle}>
+              <FacebookIcon
+                className={styles.socialIcon}
+                fontSize='small'
+                color='inherit'
+              />
+            </span>
+          </a>
+          <a href="https://www.youtube.com/watch?v=_65kfGhZXZw" target="_blank">
+            <span className={styles.socialIconCircle}>
+              <TwitterIcon
+                className={styles.socialIcon}
+                fontSize='small'
+                color='inherit'
+              />
+            </span>
+          </a>
+          <a href="https://www.youtube.com/watch?v=_65kfGhZXZw" target="_blank">
+            <span className={styles.socialIconCircle}>
+              <InstagramIcon
+                className={styles.socialIcon}
+                fontSize='small'
+                color='inherit'
+              />
+            </span>
+          </a>
+        </div>
+        <div>
+          <p className={styles.copyright}> © 2020 Majungasaurus. All rights reserved.</p>
+        </div>
+      </div>
+    )
+  }
+}
